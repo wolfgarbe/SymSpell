6c74fdbb357457af562333cd70623f4e7d81063e
softwx <softwxgit@gmail.com>
2018-04-07 22:31:05 -0600

    Update to latest version of SoftWx.Match.

---
+++ symspell/SoftWx.Match/IDistance.cs
@@ -0,0 +1,44 @@
+﻿// Copyright ©2015-2018 SoftWx, Inc.
+// Released under the MIT License the text of which appears at the end of this file.
+// <authors> Steve Hatchett
+
+namespace SoftWx.Match {
+    /// <summary>Types implementing the IDistance interface provide methods
+    /// for computing a relative distance between two strings.</summary>
+    public interface IDistance {
+        /// <summary>Return a measure of the distance between two strings.</summary>
+        /// <param name="string1">One of the strings to compare.</param>
+        /// <param name="string2">The other string to compare.</param>
+        /// <returns>0 if the strings are equivalent, otherwise a positive number whose
+        /// magnitude increases as difference between the strings increases.</returns>
+        double Distance(string string1, string string2);
+
+        /// <summary>Return a measure of the distance between two strings.</summary>
+        /// <param name="string1">One of the strings to compare.</param>
+        /// <param name="string2">The other string to compare.</param>
+        /// <param name="maxDistance">The maximum distance that is of interest.</param>
+        /// <returns>-1 if the distance is greater than the maxDistance, 0 if the strings
+        /// are equivalent, otherwise a positive number whose magnitude increases as
+        /// difference between the strings increases.</returns>
+        double Distance(string string1, string string2, double maxDistance);
+    }
+}
+/*
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
